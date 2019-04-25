b2e16d46b7f88743439d77bc0ca8d6bb5737ab41
jordanmmartin <jdordan23@gmail.com>
2019-04-25 12:37:21 -0400

    cleaned up components and added comments

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,36 +2,37 @@ import React, { Component } from 'react'
 
 class Card extends Component {
 
-showColor = () => {
-  if(this.props.cardObj.suit === 'clubs' || this.props.cardObj.suit === 'spades'){
-    return 'black'
-  } else {
-    return 'red'
+  // Checks the cardObj in props to conditionally return a color to use in className
+  showColor = () => {
+    if(this.props.cardObj.suit === 'clubs' || this.props.cardObj.suit === 'spades'){
+      return 'black'
+    } else {
+      return 'red'
+    }
   }
-}
 
-showSuit = () => {
-  if(this.props.cardObj.suit === 'clubs'){
-    return <p className={`card-suit-center ${this.showColor()}`}>&clubs;</p>
-  } else if(this.props.cardObj.suit === 'spades'){
-    return <p className={`card-suit-center ${this.showColor()}`}>&spades;</p>
-  } else if(this.props.cardObj.suit === 'hearts'){
-    return <p className={`card-suit-center ${this.showColor()}`}>&hearts;</p>
-  } else if(this.props.cardObj.suit === 'diamonds'){
-    return <p className={`card-suit-center ${this.showColor()}`}>&diams;</p>
+  // Checks the cardObj in props to conditionally return a suit icon
+  showSuit = () => {
+    if(this.props.cardObj.suit === 'clubs'){
+      return <p className={`card-suit-center ${this.showColor()}`}>&clubs;</p>
+    } else if(this.props.cardObj.suit === 'spades'){
+      return <p className={`card-suit-center ${this.showColor()}`}>&spades;</p>
+    } else if(this.props.cardObj.suit === 'hearts'){
+      return <p className={`card-suit-center ${this.showColor()}`}>&hearts;</p>
+    } else if(this.props.cardObj.suit === 'diamonds'){
+      return <p className={`card-suit-center ${this.showColor()}`}>&diams;</p>
+    }
   }
-}
 
-render(){
-  console.log(this.props)
-  return(
-    <div className='card' id={this.props.cardObj.id}>
-      <p className={`card-rank-top-left ${this.showColor()}`}>{this.props.cardObj.rank}</p>
-      {this.showSuit()}
-      <p className={`card-rank-bottom-right ${this.showColor()}`}>{this.props.cardObj.rank}</p>
-    </div>
-  )
-}
+  render(){
+    return(
+      <div className='card' id={this.props.cardObj.id}>
+        <p className={`card-rank-top-left ${this.showColor()}`}>{this.props.cardObj.rank}</p>
+        {this.showSuit()}
+        <p className={`card-rank-bottom-right ${this.showColor()}`}>{this.props.cardObj.rank}</p>
+      </div>
+    )
+  }
 }
 
 export default Card
--- src/components/Table.js
+++ src/components/Table.js
@@ -9,51 +9,51 @@ class Table extends Component {
     hand: []
   }
 
-handleCardDeal = () =>{
-  console.log('DEALING CARDS...');
-  fetch(`http://localhost:3000/decks/${this.state.deck.id}/deal`)
-    .then(r => r.json())
-    // .then(console.log)
-    .then(r => this.setState({
-      hand: r.decks,
-      showCards: true
-      }))
-}
-
-displayHand = () => {
-  let hand = this.state.hand.map(card => <Card cardObj={card} key={card.id}/>)
-  return hand
-}
-
-componentDidMount(){
-  const options = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json"
-      },
-      body: JSON.stringify({})
+  //When the 'Deal Cards!' button is clicked, a fetch request is sent to get 5 random cards from a specific decks
+  handleCardDeal = () =>{
+    fetch(`http://localhost:3000/decks/${this.state.deck.id}/deal`)
+      .then(r => r.json())
+      .then(r => this.setState({
+        hand: r.decks,
+        showCards: true
+        }))
   }
-  fetch('http://localhost:3000/decks', options)
-    .then(r => r.json())
-    .then(r => this.setState({
-      deck: r.deck
-      }))
-}
 
-render(){
-  console.log(this.state);
-  return(
-      <div className='main'>
-        <div className='table'>
-          <div className='board'>
-            {this.state.showCards && this.displayHand()}
+  // Returns a Card component for each of the card objects in hand array of state
+  displayHand = () => {
+    let hand = this.state.hand.map(card => <Card cardObj={card} key={card.id}/>)
+    return hand
+  }
+
+  //Once the Table component mounts, a fetch request is sent to create a new deck
+  componentDidMount(){
+    const options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json"
+        },
+        body: JSON.stringify({})
+    }
+    fetch('http://localhost:3000/decks', options)
+      .then(r => r.json())
+      .then(r => this.setState({
+        deck: r.deck
+        }))
+  }
+
+  render(){
+    return(
+        <div className='main'>
+          <div className='table'>
+            <div className='board'>
+              {this.state.showCards && this.displayHand()}
+            </div>
           </div>
+          {!this.state.showCards && <button onClick={this.handleCardDeal} className='deal-button'>Deal Cards!</button>}
         </div>
-        {!this.state.showCards && <button onClick={this.handleCardDeal} className='deal-button'>Deal Cards!</button>}
-      </div>
-  )
-}
+    )
+  }
 }
 
 export default Table
